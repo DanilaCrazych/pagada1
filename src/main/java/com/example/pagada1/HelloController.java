8960f4f1f3be529deac05dc6ab1289bcb7736e2a
DanilaCrazych <Mag7istr@mail.ru>
2022-11-09 00:22:54 +0300

    Initial commit

---
+++ src/main/java/com/example/pagada1/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.pagada1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
